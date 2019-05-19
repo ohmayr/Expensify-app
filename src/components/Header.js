5e775a10e28cd76547c7495bba8e07918e0f4b6d
ohmayr <omairnaveed@ymail.com>
2019-05-19 17:39:29 -0400

    firebase settings

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.title}</h1>
-      {props.subtitle && <h2>{props.subtitle}</h2>}
-    </div>
-  );
-};
-
-Header.defaultProps = {
-  title: 'Indecision'
-};
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
+  </header>
+);
 
 export default Header;
